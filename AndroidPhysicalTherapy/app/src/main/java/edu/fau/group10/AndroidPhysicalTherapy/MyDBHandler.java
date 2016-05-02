12bb72cb291e6f8a7931f9bd416c47ed2b92bfb2
fpsoares <fsoares2014@fau.edu>
2016-05-01 23:13:03 -0400

    Upload final app version, expanded in folder

---
+++ AndroidPhysicalTherapy/app/src/main/java/edu/fau/group10/AndroidPhysicalTherapy/MyDBHandler.java
@@ -0,0 +1,119 @@
+package edu.fau.group10.AndroidPhysicalTherapy;
+
+/**
+ * Created by myers on 4/27/2016.
+ */
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.Cursor;
+import android.content.Context;
+import android.content.ContentValues;
+import android.util.Log;
+
+public class MyDBHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "users.db";
+    public static final String TABLE_USERS = "users";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_USERNAME = "username";
+    public static final String COLUMN_VISTA = "vista";
+    public static final String COLUMN_PASSWORD = "password";
+
+    public MyDBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_USERS + "(" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_USERNAME + " TEXT, " +
+                COLUMN_VISTA + " TEXT, " +
+                COLUMN_PASSWORD + " TEXT " +
+                ");";
+        db.execSQL(query);
+    }
+
+    //needed only if table is upgraded (new columns need to be added, removed, etc)
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+        onCreate(db);
+    }
+
+    //Add new rows to the database
+    public void addUser(Users user) {
+        // ContentValues values = new ContentValues();
+        //values.put(COLUMN_USERNAME, user.get_username());
+        //values.put(COLUMN_PASSWORD, user.get_password());
+        //values.put(COLUMN_VISTA, user.get_vista());
+
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("INSERT INTO USERS (username, password, vista) VALUES ('" + user.get_username() + "','" + user.get_password() + "','" + user.get_vista() + "');");
+        db.close();
+    }
+
+    //delete users from the database
+    public void deleteUser(String username) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("DELETE FROM " + TABLE_USERS + " WHERE" + COLUMN_USERNAME + "=\"" + username + "\";");
+
+    }
+
+    public String getSingleEntry(String userName)
+    {
+        SQLiteDatabase db = getWritableDatabase();
+
+
+        Cursor cursor = db.query("USERS", null, " username=?", new String[]{userName}, null, null, null);
+        if(cursor.getCount()<1) // UserName Not Exist
+        {
+            cursor.close();
+            return "Username does not exist - Try again";
+        }
+        cursor.moveToFirst();
+        String password = cursor.getString(3);
+
+        //cursor.getString(cursor.getColumnIndex("password"));
+        cursor.close();
+        return password;
+    }
+
+    public Boolean checkforUser(String username)
+    {
+        SQLiteDatabase db = getWritableDatabase();
+        Cursor cursor = db.query("USERS", null, " username=?", new String[]{username}, null, null, null);
+        if(cursor.getCount()<1) // UserName Not Exist
+        {
+            return true;
+        }
+        else return false;
+    }
+
+
+
+    //Print database as string for testing
+    public String databaseToString() {
+        String dbString = "";
+        SQLiteDatabase db = getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_USERS + " WHERE 1";
+
+        //cursor points to the location in results(i.e. row 1)
+        Cursor c = db.rawQuery(query, null);
+        c.moveToFirst();
+        String user = c.getString(2);
+
+
+        while (!c.isAfterLast()) {
+            if (c.getString(c.getColumnIndex("username")) != null) {
+                dbString += c.getString(c.getColumnIndex("username"));
+                dbString += "\n";
+            }
+        }
+        db.close();
+        //return dbString;
+        return user;
+    }
+
+}
